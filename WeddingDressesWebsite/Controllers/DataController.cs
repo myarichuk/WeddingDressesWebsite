38600ce0cf67064af8a22bce105d4d65b6bc40a5
Michael Yarichuk <michael.yarichuk@gmail.com>
2015-03-16 02:20:34 +0200

    finished server-side catalog related stuff. client-side, the catalog kind of works. probably I should change gallery plugin

--- WeddingDressesWebsite/Controllers/DataController.cs
+++ WeddingDressesWebsite/Controllers/DataController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Web.Hosting;
@@ -13,6 +18,7 @@ namespace WeddingDressesWebsite.Controllers
 	public class DataController : ApiController
 	{
 		private const string imageExtension = ".jpg";
+		private const string imageExtension2 = ".jpeg";
 
 		[Route("data/main-page-images")]
 		public IEnumerable<string> GetImagesForMainPage()
@@ -21,16 +27,52 @@ namespace WeddingDressesWebsite.Controllers
             const string mainPageImagesPath = "~/Content/Images/Main";
 
 			var fileUrls = Directory.EnumerateFiles(HostingEnvironment.MapPath(mainPageImagesPath),"*",SearchOption.AllDirectories)
-								    .Where(path => path.EndsWith(imageExtension, StringComparison.InvariantCultureIgnoreCase))
+								    .Where(path => path.EndsWith(imageExtension, StringComparison.InvariantCultureIgnoreCase) ||
+												   path.EndsWith(imageExtension2, StringComparison.InvariantCultureIgnoreCase))
 								    .Select(path => Uri.EscapeUriString(urlPrefix + new FileInfo(path).Name));
 			
             return fileUrls;
 		}
 
+		[Route("images/catalog")]
+		public HttpResponseMessage GetCatalogImage(string url, int? width)
+		{
+			if (string.IsNullOrWhiteSpace(url))	//cannot translate absolute uri to path
+				return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+			var basePath = HostingEnvironment.MapPath("~/");
+			var imagePath = basePath + Path.DirectorySeparatorChar + url.Replace('/', Path.DirectorySeparatorChar);
+
+			var fileInfo = new FileInfo(imagePath);
+			if (fileInfo.Exists == false)
+				return new HttpResponseMessage(HttpStatusCode.NotFound);
+
+			var imageBytes = File.ReadAllBytes(imagePath);
+			var result = new HttpResponseMessage(HttpStatusCode.OK);
+			using (var inStream = new MemoryStream(imageBytes))
+			{
+				using (var outStream = new MemoryStream())
+				{
+					using (var imageFactory = new ImageFactory())
+					{
+						imageFactory.Load(inStream)
+									.Resize(new Size(width ?? 0,0))
+									.Format(new JpegFormat())
+									.Quality(70)
+									.Save(outStream);
+					}
+
+					result.Content = new ByteArrayContent(outStream.ToArray());
+					result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+				}
+			}
+
+			return result;
+		}
+
 		[Route("data/catalog-page-images")]
 		public IEnumerable<dynamic> GetImagesForCatalog()
 		{
-			const string urlPrefix = "/content/images/catalog/";
 			const string mainPageImagesPath = "~/Content/Images/Catalog";
 
 			var fileInfos = Directory.EnumerateFiles(HostingEnvironment.MapPath(mainPageImagesPath), "*", SearchOption.AllDirectories)
